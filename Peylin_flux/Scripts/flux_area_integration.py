f229710524a32ba7e27ac74fb15c7c6abb70f512
Ross Ursino <rursino@student.unimelb.edu.au>
2019-07-09 17:29:37 +1000

    Completed first integration with time for Rayner dataset.

--- Peylin_flux/Scripts/flux_area_integration.py
+++ Peylin_flux/Scripts/flux_area_integration.py
@@ -60,9 +60,7 @@ JAMSTEC = './../data/fco2_JAMSTEC-V1-2-2018_June2018-ext3_1996-2017_monthlymean_
 JENA_S76 = './../data/fco2_JENA-s76-4-2-2018_June2018-ext3_1976-2017_monthlymean_XYT.nc'
 JENA_S85 = './../data/fco2_JENA-s85-4-2-2018_June2018-ext3_1985-2017_monthlymean_XYT.nc'
 
-data = Rayner
-df = xr.open_dataset(data)
-
+df = xr.open_dataset(Rayner)
 
 # Obtain object names for variables.
 if 'latitude' and 'longitude' and 'time' in df.variables:
@@ -77,28 +75,14 @@ trop_grid_area = earth_area_grid(lat[(lat>-23) & (lat<23)], lon)
 north_grid_area = earth_area_grid(lat[lat>23], lon)
 
 
-def single_time(time_index, write_output=False):
+def spatial_integration(data, variables, time=None):
     
-    "Obtain spaitally aggregated fluxes at a specified time point."
+    "Return a dataframe of total sinks for specific regions and the globe and at a specific time point or a range of time points. The regions are split into land and ocean and are latitudinally split as follows: 90 degN to 23 degN, 23 degN to 23 degS and 23 degS to 90 degS. Globally integrated fluxes are also included for each of land and ocean."
     
-    # Obtain a grid of land and ocean fluxes at a time point.
-    time_point = df.time[time_index]
-    earth_land_flux = df.Terrestrial_flux.sel(time=time_point).values
-    earth_ocean_flux = df.ocean.sel(time=time_point).values
-
-    # Obtain a grid of total sink
-    earth_land_sink = earth_grid_area*earth_land_flux
-    south_land_sink = south_grid_area*earth_land_flux[lat<-23]
-    trop_land_sink = trop_grid_area*earth_land_flux[(lat>-23) & (lat<23)]
-    north_land_sink = north_grid_area*earth_land_flux[lat>23]
-
-    earth_ocean_sink = earth_grid_area*earth_ocean_flux
-    south_ocean_sink = south_grid_area*earth_ocean_flux[lat<-23]
-    trop_ocean_sink = trop_grid_area*earth_ocean_flux[(lat>-23) & (lat<23)]
-    north_ocean_sink = north_grid_area*earth_ocean_flux[lat>23]
+    # Read dataset.
+    df = xr.open_dataset(data)
     
-    # Obtain total values of sinks in globe and regions.
-    
+    # Initialise a dataframe of the regional flux values.
     total_values = pd.DataFrame(columns=
                                 ['time',
                                  'earth_land_total',
@@ -111,57 +95,80 @@ def single_time(time_index, write_output=False):
                                  'north_ocean_total']
                                )
     
-    total_values.loc[0,:] = np.array([time_index*1e15,
-                                      np.sum(earth_land_sink),
-                                      np.sum(south_land_sink),
-                                      np.sum(trop_land_sink),
-                                      np.sum(north_land_sink),
-                                      np.sum(earth_ocean_sink),
-                                      np.sum(south_ocean_sink),
-                                      np.sum(trop_ocean_sink),
-                                      np.sum(north_ocean_sink)])*1e-15
+    # Check that time range (min_time to max_time) is within the time range of the dataset.
+    if time == None:
+        min_time = range(df.time.size)[0]
+        max_time = range(df.time.size)[-1] + 1
+    
+    if type(time) == list or type(time) == tuple:
+        min_time = time[0]
+        max_time = time[1] + 1
+    elif type(time) == int or type(time) == float:
+        min_time = int(time)
+        max_time = int(time) + 1
+    
+    arg_time_range = range(min_time, max_time)
+    df_time_range = range(df.time.size)
+    if not (arg_time_range[0] in df_time_range and arg_time_range[-1] in df_time_range):
+        raise ValueError('Time points are out of bounds of dataframe.')
     
     
-    # Return an output file of total sink if requested.
-    
-    if write_output:
-        output = open("output.txt", "w")
-        output.write(
-            "netCDF file path: " + data +"\n\n\n"
-            "Time: " + str(time_point.values)[:10] + "\n\n"
-            "Earth Land Total Sink: " + str(total_values.loc[0,'earth_land_total']) +" GtC\n"
-            "South Land Total Sink: " + str(total_values.loc[0,'south_land_total']) +" GtC\n"
-            "Tropical Land Total Sink: " + str(total_values.loc[0,'trop_land_total']) +" GtC\n"
-            "North Land Total Sink: " + str(total_values.loc[0,'north_land_total']) +" GtC\n\n"
-            "Earth Ocean Total Sink: " + str(total_values.loc[0,'earth_ocean_total']) +" GtC\n"
-            "South Ocean Total Sink: " + str(total_values.loc[0,'south_ocean_total']) +" GtC\n"
-            "Tropical Ocean Total Sink: " + str(total_values.loc[0,'trop_ocean_total']) +" GtC\n"
-            "North Ocean Total Sink: " + str(total_values.loc[0,'north_ocean_total']) +" GtC\n"
-        )
-        output.close()
-    
-    return total_values
+    # Aggregate fluxes spatially at each time point and insert into dataframe.
+    for time_index in range(min_time, max_time):
+        
+        # Obtain a grid of land and ocean fluxes at a time point.
+        time_point = df.time[time_index]
+        earth_land_flux = df[variables[0]].sel(time=time_point).values
+        earth_ocean_flux = df[variables[1]].sel(time=time_point).values
 
+        # Obtain a grid of total sink
+        earth_land_sink = earth_grid_area*earth_land_flux
+        south_land_sink = south_grid_area*earth_land_flux[lat<-23]
+        trop_land_sink = trop_grid_area*earth_land_flux[(lat>-23) & (lat<23)]
+        north_land_sink = north_grid_area*earth_land_flux[lat>23]
 
+        earth_ocean_sink = earth_grid_area*earth_ocean_flux
+        south_ocean_sink = south_grid_area*earth_ocean_flux[lat<-23]
+        trop_ocean_sink = trop_grid_area*earth_ocean_flux[(lat>-23) & (lat<23)]
+        north_ocean_sink = north_grid_area*earth_ocean_flux[lat>23]
 
-def multiple_time():
-    
-    "Return a dataframe of total sinks for a range of time points."
-    
-    total_values = pd.DataFrame(columns=
-                                ['time',
-                                 'earth_land_total',
-                                 'south_land_total',
-                                 'trop_land_total',
-                                 'north_land_total',
-                                 'earth_ocean_total',
-                                 'south_ocean_total',
-                                 'trop_ocean_total',
-                                 'north_ocean_total']
-                               )
-    
-    for i in range(df.time.size):
-        single = single_time(i)
-        total_values.loc[i,:] = np.array(single.loc[0])
+        # Obtain total values of sinks in globe and regions.
+        total_values.loc[time_index,:] = np.array([df.time.values[time_index],
+                                          np.sum(earth_land_sink*1e-15),
+                                          np.sum(south_land_sink*1e-15),
+                                          np.sum(trop_land_sink*1e-15),
+                                          np.sum(north_land_sink*1e-15),
+                                          np.sum(earth_ocean_sink*1e-15),
+                                          np.sum(south_ocean_sink*1e-15),
+                                          np.sum(trop_ocean_sink*1e-15),
+                                          np.sum(north_ocean_sink*1e-15)])
+        
     
     return total_values.reset_index(drop=True)
+
+
+
+def row_output(df, time_index, data_path=None):
+    
+    "Write a text file of total sinks at a specific time point."
+    
+    if data_path:
+        data_line = "netCDF file path: " + data_path +"\n"
+    else:
+        data_line = ""
+    
+    output = open("output.txt", "w")
+    output.write(
+        
+        data_line +"\n\n"
+        "Time: " + str(df.loc[time_index,'time']) + "\n\n"
+        "Earth Land Total Sink: " + str(df.loc[time_index,'earth_land_total']) +" GtC\n"
+        "South Land Total Sink: " + str(df.loc[time_index,'south_land_total']) +" GtC\n"
+        "Tropical Land Total Sink: " + str(df.loc[time_index,'trop_land_total']) +" GtC\n"
+        "North Land Total Sink: " + str(df.loc[time_index,'north_land_total']) +" GtC\n\n"
+        "Earth Ocean Total Sink: " + str(df.loc[time_index,'earth_ocean_total']) +" GtC\n"
+        "South Ocean Total Sink: " + str(df.loc[time_index,'south_ocean_total']) +" GtC\n"
+        "Tropical Ocean Total Sink: " + str(df.loc[time_index,'trop_ocean_total']) +" GtC\n"
+        "North Ocean Total Sink: " + str(df.loc[time_index,'north_ocean_total']) +" GtC\n"
+    )
+    output.close()
