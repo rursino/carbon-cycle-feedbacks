5383955755958936fb37ca5c5eae85f6302bbd6c
rursino <rossursino@hotmail.com>
2020-06-08 14:32:55 +1000

    Started TRENDY SpatialAgg devs

--- scripts/devs/TRENDY.py
+++ scripts/devs/TRENDY.py
@@ -13,6 +13,57 @@ fname = "./../../data/TRENDY/models/LPJ-GUESS/S0/LPJ-GUESS_S0_cVeg.nc"
 
 
 """ FUNCTIONS """
+def scalar_earth_area(df, minlat, maxlat, minlon, maxlon):
+    """Returns the area of earth in the defined grid box."""
+
+    r_earth = 6.371e6 # Radius of Earth
+    dtor = np.pi / 180. # conversion from degrees to radians.
+
+    diff_lon = np.unwrap((minlon, maxlon), discont=360.+1e-6)
+
+    return 2. * np.pi * r_earth**2 * (np.sin(dtor * maxlat) -
+    np.sin(dtor * minlat)) * (diff_lon[1] - diff_lon[0]) / 360.
+
+
+def earth_area(df, minlat, maxlat, minlon, maxlon):
+    """Returns grid of areas with shape=(minlon, minlat) and earth area.
+    """
+
+    r_earth = 6.371e6 # Radius of Earth
+    dtor = np.pi / 180. # conversion from degrees to radians.
+
+    result = np.zeros((np.array(minlon).size, np.array(minlat).size))
+    diffsinlat = 2. * np.pi * r_earth**2 *(np.sin(dtor*maxlat) -
+    np.sin(dtor * minlat))
+    diff_lon = np.unwrap((minlon, maxlon), discont=360.+1e-6)
+
+    for i in range(np.array(minlon).size):
+        result[i, :] = diffsinlat *(diff_lon[1,i]-diff_lon[0,i])/360.
+
+    return result
+
+def earth_area_grid(df, lats, lons):
+    """Returns an array of the areas of each grid box within a defined set of
+    lats and lons.
+    """
+
+    result = np.zeros((lats.size, lons.size))
+
+    minlats = lats - 0.5*(lats[1] - lats[0])
+    maxlats= lats + 0.5*(lats[1] - lats[0])
+    minlons = lons - 0.5*(lons[1] - lons[0])
+    maxlons= lons + 0.5*(lons[1] - lons[0])
+
+    for i in range(lats.size):
+        result[i,:] = scalar_earth_area(df,
+        minlats[i],
+        maxlats[i],
+        minlons[0],
+        maxlons[0]
+        )
+
+    return result
+
 def spatial_integration(df, start_time=None, end_time=None,
 lat_split=30, lon_range=None):
     """Returns a xr.Dataset of total global and regional sinks at a
@@ -25,17 +76,15 @@ lat_split=30, lon_range=None):
     Parameters
     ==========
 
-    start_time: string, optional
+    start_time: int, optional
 
-        The month and year of the time to start the integration in the
-        format '%Y-%M'.
+        The year of the time to start the integration. Must be an integer.
         Default is None.
 
-    end_time: string, optional
+    end_time: int, optional
 
-        The month and year of the time to end the integration in the
-        format '%Y-%M'. Note that the integration will stop the month
-        before argument.
+        The year of the time to end the integration. Must be an integer. Note
+        that the integration will stop the year before argument.
         Default is None.
 
     lat_split: integer, optional
@@ -57,34 +106,29 @@ lat_split=30, lon_range=None):
     """
 
     if start_time == None:
-        start_time = df.time.values[0].strftime('%Y-%m')
+        start_time = df.time.values[0].strftime('%Y')
+
     if end_time == None:
         end_time = df.time.values[-1]
-        try:
-            next_month = end_time.replace(month=end_time.month+1)
-        except ValueError:
-            next_month = end_time.replace(year=end_time.year+1, month=1)
-
-        end_time = next_month.strftime('%Y-%m')
+        end_time = (
+                    end_time
+                        .replace(year=end_time.year+1)
+                        .strftime('%Y')
+                    )
 
     arg_time_range = (
     pd
-        .date_range(start=start_time, end=end_time, freq='M')
-        .strftime('%Y-%m')
+        .date_range(start=start_time, end=end_time, freq='Y')
+        .strftime('%Y')
     )
 
     lat = df.latitude
     lon = df.longitude
-    earth_grid_area = self.earth_area_grid(lat,lon)
 
-    days = {'01': 31, '02': 28, '03': 31, '04': 30,
-            '05': 31, '06': 30, '07': 31, '08': 31,
-            '09': 30, '10': 31, '11': 30, '12': 31}
+    earth_grid_area = earth_area_grid(df, lat, lon)
 
     values = {
-    "Earth_Land": [], "South_Land": [], "Tropical_Land": [],
-    "North_Land": [], "Earth_Ocean": [], "South_Ocean": [],
-    "Tropical_Ocean": [], "North_Ocean": []}
+    "Earth": [], "South": [], "Tropical": [], "North": []}
 
     time_vals = []
 
@@ -93,39 +137,13 @@ lat_split=30, lon_range=None):
     (lat>-lat_split) & (lat<lat_split), lat>lat_split
     )
 
-
     for time_point in arg_time_range:
-
-        days_in_month = days[time_point[-2:]]
-
-        earth_land_flux = (
-        df['Terrestrial_flux']
-            .sel(time=time_point).values[0]*(days_in_month/365)
-            )
-
-        # Rayner has ocean variable as 'ocean' instead of 'Ocean_flux'.
-        try:
-            earth_ocean_flux = (
-            df['Ocean_flux']
-                .sel(time=time_point).values[0]*(days_in_month/365)
-                )
-        except KeyError:
-            earth_ocean_flux = (
-            df['ocean']
-                .sel(time=time_point).values[0]*(days_in_month/365)
-                )
-
-        earth_land_sink = earth_grid_area*earth_land_flux
-        earth_ocean_sink = earth_grid_area*earth_ocean_flux
+        flux = df['cVeg'].sel(time=time_point).values[0]
+        sink = earth_grid_area * flux
 
         condition = iter(lat_conditions)
         for var in list(values.keys())[:4]:
-            sum = np.sum(1e-15*earth_land_sink[next(condition)])
-            values[var].append(sum)
-
-        condition = iter(lat_conditions)
-        for var in list(values.keys())[4:]:
-            sum = np.sum(1e-15*earth_ocean_sink[next(condition)])
+            sum = np.sum(1e-15 * sink[next(condition)]) #fix nan
             values[var].append(sum)
 
         time_vals.append(df.sel(time=time_point).time.values[0])
@@ -140,4 +158,5 @@ lat_split=30, lon_range=None):
 
 """ EXECUTION """
 df = xr.open_dataset(fname)
-spatial_integration(df)
+res = spatial_integration(df)
+res
