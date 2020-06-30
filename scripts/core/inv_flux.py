33f6ddb0a43d9df75b1611733f543f99399cd612
rursino <rossursino@hotmail.com>
2020-06-30 11:40:40 +1000

    Testing INVF

--- scripts/core/inv_flux.py
+++ scripts/core/inv_flux.py
@@ -39,7 +39,8 @@ class SpatialAgg:
             read_file = open(data, 'rb')
             _data = pickle.load(read_file)
             if not (isinstance(_data, xr.Dataset) or isinstance(_data, xr.DataArray)):
-                raise TypeError("Pickle object must be of type xr.Dataset or xr.DataArray.")
+                raise TypeError("Pickle object must be of\
+                 type xr.Dataset or xr.DataArray.")
 
         else:
             _data = xr.open_dataset(data)
