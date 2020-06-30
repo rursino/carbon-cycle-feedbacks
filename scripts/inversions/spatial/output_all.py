baf597cdeabb4c96ce17188ad662aac597ab8c46
rursino <rossursino@hotmail.com>
2020-06-30 14:40:54 +1000

    Improvement of output_all code

--- scripts/inversions/spatial/output_all.py
+++ scripts/inversions/spatial/output_all.py
@@ -1,48 +1,41 @@
-""" Outputs dataframes of spatial, yearly, decadal and whole time integrations (for all globe and regions) for each model.
-Output format is binary csv through the use of pickle.
-
-Run this script from the bash shell. """
+""" Outputs dataframes of spatial, yearly, decadal and whole time integrations
+(for all globe and regions) for each model. Output format is binary csv
+through the use of pickle.
+Run this script from the bash shell.
+"""
 
 import sys
 sys.path.append("./../core/")
+import inv_flux
 
 import os
-import inv_flux
 import pickle
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
     output_folder = sys.argv[2]
 
-    if input_file.endswith(".pik"):
-        input_file = pickle.load(open(input_file, 'rb'))
+    df = (inv_flux
+            .SpatialAgg(data = pickle.load(open(input_file, 'rb')))
+            .spatial_integration()
+         )
 
-    df = inv_flux.SpatialAgg(data=input_file)
+    arrays = {
+        "month": df,
+        "year": df.resample({'time': 'Y'}).sum(),
+        "decade": df.resample({'time': '10Y'}).sum(),
+        "whole": df.sum()
+    }
 
-    df_spatial = df.spatial_integration()
-    df_year = df_spatial.resample({'time': 'Y'}).sum()
-    df_decade = df_spatial.resample({'time': '10Y'}).sum()
-    df_whole = df_spatial.sum()
-
-
-    try:
-        os.mkdir(output_folder)
-    except OSError:
-        if os.path.isdir(output_folder):
-            print("Directory %s already exists" % output_folder)
+    if os.path.isdir(output_folder):
+        print("Directory %s already exists" % output_folder)
     else:
-        print ("Successfully created the directory %s " % output_folder)
-
+        os.mkdir(output_folder)
+        print("Successfully created the directory %s " % output_folder)
 
     # Output files after directory successfully created.
-    pickle.dump(df_spatial, open("{}/spatial.pik".format(output_folder), "wb"))
-    print ("Successfully created %s/spatial.pik " % output_folder)
+    for freq in arrays:
+        array = arrays[freq]
 
-    pickle.dump(df_year, open("{}/year.pik".format(output_folder), "wb"))
-    print ("Successfully created %s/year.pik " % output_folder)
-
-    pickle.dump(df_decade, open("{}/decade.pik".format(output_folder), "wb"))
-    print ("Successfully created %s/decade.pik" % output_folder)
-
-    pickle.dump(df_whole, open("{}/whole_time.pik".format(output_folder), "wb"))
-    print ("Successfully created %s/whole_time.pik" % output_folder)
+        pickle.dump(array, open(f"{output_folder}/{freq}.pik", "wb"))
+        print(f"Successfully created {output_folder}/{freq}.pik")
