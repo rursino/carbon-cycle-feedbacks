a46f1bb9923f298bc1d690822d0c376e8db06f4c
rursino <rossursino@hotmail.com>
2020-07-02 16:23:10 +1000

    Work with tests

--- scripts/core/tests/test_inv_flux.py
+++ scripts/core/tests/test_inv_flux.py
@@ -12,11 +12,9 @@ import xarray as xr
 from cftime import Datetime360Day
 
 """ SETUP """
-ds, basic_result = None, None
-
 def setup_module(module):
     print('--------------------setup--------------------')
-    global ds, basic_result
+    global test_ds, basic_test_result
 
     ds = xr.open_dataset("./../../../data/inversions/fco2_CAMS-V17-1-2018_June2018-ext3_1979-2017_monthlymean_XYT.nc")
 
@@ -33,12 +31,29 @@ def setup_module(module):
                 coords = {'longitude': lon, 'latitude': lat, 'time': time}
                 )
 
-    ds = invf.SpatialAgg(testData)
-    basic_result = ds.spatial_integration()
+    test_ds = invf.SpatialAgg(testData)
+    basic_test_result = test_ds.spatial_integration()
 
 
 """ TESTS """
 def test_check_instance():
-    assert isinstance(basic_result, xr.Dataset)
+    assert isinstance(basic_test_result, xr.Dataset)
 
-def test_spatial_integration():
+def test_regions_add_to_global():
+    land_components = (basic_test_result.South_Land.values +
+                       basic_test_result.Tropical_Land.values +
+                       basic_test_result.North_Land.values).sum()
+    ocean_components = (basic_test_result.South_Ocean.values +
+                        basic_test_result.Tropical_Ocean.values +
+                        basic_test_result.North_Ocean.values).sum()
+
+    assert land_components == basic_test_result.Earth_Land.values.sum()
+    assert ocean_components == basic_test_result.Earth_Ocean.values.sum()
+
+def test_spatial_sum():
+    earth_surface_area = 4 * np.pi * (test_ds.earth_radius ** 2)
+
+    total_flux = (basic_test_result.Earth_Land.values +
+                  basic_test_result.Earth_Ocean.values).sum()
+
+    assert total_flux == earth_surface_area * 1e-15
