040e31c97259b09c2af8071780b76f43575e0d8f
rursino <rossursino@hotmail.com>
2020-07-06 20:08:55 +1000

    further tests to invf

--- scripts/core/tests/test_inv_flux.py
+++ scripts/core/tests/test_inv_flux.py
@@ -11,10 +11,13 @@ import numpy as np
 import xarray as xr
 from cftime import Datetime360Day
 
+import pytest
+
 """ SETUP """
 def setup_module(module):
     print('--------------------setup--------------------')
-    global test_ds, basic_test_result, lat, lon, land_vals, ocean_vals
+    global original_ds, test_ds
+    global basic_test_result, lat, lon, land_vals, ocean_vals
 
     ds = xr.open_dataset("./../../../data/inversions/fco2_CAMS-V17-1-2018_June2018-ext3_1979-2017_monthlymean_XYT.nc")
 
@@ -41,7 +44,9 @@ def setup_module(module):
                 coords = {'longitude': lon, 'latitude': lat, 'time': time}
                 )
 
+    original_ds = invf.SpatialAgg(ds)
     test_ds = invf.SpatialAgg(testData)
+
     basic_test_result = test_ds.latitudinal_splits()
 
 
@@ -50,18 +55,26 @@ def test_check_instance():
     assert isinstance(basic_test_result, xr.Dataset)
 
 def test_regions_add_to_global():
-    land_components = (basic_test_result.South_Land.values +
-                       basic_test_result.Tropical_Land.values +
-                       basic_test_result.North_Land.values).sum()
-    ocean_components = (basic_test_result.South_Ocean.values +
-                        basic_test_result.Tropical_Ocean.values +
-                        basic_test_result.North_Ocean.values).sum()
 
-    basic_land_values = basic_test_result.Earth_Land.values.sum()
-    basic_ocean_values = basic_test_result.Earth_Ocean.values.sum()
+    def differences(dataset):
 
-    assert abs(land_components - basic_land_values) < 1
-    assert abs(ocean_components - basic_ocean_values) < 1
+        land_components = (dataset.South_Land.values +
+                           dataset.Tropical_Land.values +
+                           dataset.North_Land.values).sum()
+        ocean_components = (dataset.South_Ocean.values +
+                            dataset.Tropical_Ocean.values +
+                            dataset.North_Ocean.values).sum()
+
+        land_values = dataset.Earth_Land.values.sum()
+        ocean_values = dataset.Earth_Ocean.values.sum()
+
+        return np.array([abs(land_components - land_values),
+               abs(ocean_components - ocean_values)])
+
+    assert np.all(differences(basic_test_result) < 1)
+    assert np.all(differences(test_ds.latitudinal_splits(23)) < 1)
+    assert np.all(differences(original_ds.latitudinal_splits()) < 1)
+    assert np.all(differences(original_ds.latitudinal_splits(23)) < 1)
 
 def test_earth_area_grid_equals_surface_area():
     earth_surface_area = 4 * np.pi * (test_ds.earth_radius ** 2)
@@ -70,13 +83,18 @@ def test_earth_area_grid_equals_surface_area():
     assert abs(earth_surface_area - expected_result) < 1
 
 def test_spatial_sum():
-    one_month_result = basic_test_result.sel(time = "1993-01")
 
-    earth_surface_area = 4 * np.pi * (test_ds.earth_radius ** 2)
+    def differences(dataset):
+        one_month_result = dataset.sel(time = "1993-01")
 
-    total_flux = (one_month_result.Earth_Land.values +
-                  one_month_result.Earth_Ocean.values).sum()
+        earth_surface_area = 4 * np.pi * (test_ds.earth_radius ** 2)
 
-    expected_result = test_ds.earth_area_grid(lat,lon).sum() * 1e-15
+        total_flux = (one_month_result.Earth_Land.values +
+                      one_month_result.Earth_Ocean.values).sum()
 
-    assert abs(total_flux - expected_result) < 1
+        expected_result = test_ds.earth_area_grid(lat,lon).sum() * 1e-15
+
+        return abs(total_flux - expected_result)
+
+    assert differences(basic_test_result) < 1
+    assert differences(test_ds.latitudinal_splits(23)) < 1
