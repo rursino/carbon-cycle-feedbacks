c7e251a5d2dee7ffc1c069d8dedc851a60417bc3
rursino <rossursino@hotmail.com>
2020-08-22 12:34:13 +1000

    setup.py added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='masters-project',
+      author='Ross Ursino',
+      author_email='rursino@student.unimelb.edu.au',
+      packages=find_packages(),
+     )
