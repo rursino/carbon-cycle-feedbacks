e8010996fa9fd25b32d2530a26772f45e25bdd60
rursino <rossursino@hotmail.com>
2020-09-12 14:15:02 +1000

    Start establishing tests for FeedbackAnalysis

---
+++ tests/test_FeedbackAnalysis.py
@@ -0,0 +1,26 @@
+""" pytest: FeedbackAnalysis module.
+"""
+
+
+""" IMPORTS """
+from core import FeedbackAnalysis
+
+import numpy as np
+import xarray as xr
+import pandas as pd
+
+import pytest
+
+""" SETUP """
+def setup_module(module):
+    print('--------------------setup--------------------')
+    global a
+
+
+
+
+""" TESTS """
+def test_whole_feedback
+def test_feedback_model
+def test_params
+def test_regstats
