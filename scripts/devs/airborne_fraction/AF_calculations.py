correct units for phi

scripts/devs/airborne_fraction/AF_calculations.py
@@ -8,18 +8,22 @@ sys.path.append('./../../core')
 import FeedbackAnalysis as FA
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+""" PHI """
+def phi(C):
+    return np.log(C/290) * 3.74 / (np.log(2) * (C - 290))
+
+phi(270)
+
+x = np.linspace(280, 840)
+y = phi(x)
+plt.plot(x,y)
+plt.ylim([0,0.3])
 
 
 """ FUNCTIONS """
-
-
-
-""" EXECUTION """
-def phi(C):
-    return np.log(C/277.15) * 3.74 / np.log(2) / (C - 277.15)
-phi(390)
-phi(840)
-
 def TRENDY(variable, timeres):
     df = FA.FeedbackOutput("TRENDY", variable, timeres)
 
@@ -58,6 +62,7 @@ def inversions(variable, timeres):
     return 1/(1 + beta + gamma)
 
 
+""" EXECUTION """
 TRENDY("Earth_Land", "month") / 2
 
 1 / (1 / inversions("Earth_Land", "month") + 1 / inversions("Earth_Ocean", "month"))

scripts/devs/airborne_fraction/GCP_AF.py
@@ -104,9 +104,9 @@ plt.plot(np.arange(60), np.array([(fossil + LUC).iloc[0] * (1.016)**i for i in r
 paramsT = params_df(GCP.index, land, ocean, co2 * 2.12, temp).sum(axis=1)
 paramsT
 
-def af_feedbacks(params, a=2, phi=0.015, rho=1.94):
+def af_feedbacks(params, a=2, phi=0.015/2.12, rho=1.94):
     b = 1 / np.log(1 + a/100)
     u = 1 - b * (params.beta + params.gamma * phi / rho)
     return 1 / u
 
-af_feedbacks(paramsT, 1)
+af_feedbacks(paramsT, 2)